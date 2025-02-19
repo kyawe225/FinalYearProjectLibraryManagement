ae08b836179bb7681bc024700c8e8ad4125bb158
kyawzinhtet <kyawe225@gmail.com>
2025-02-19 14:54:12 +0700

    Initial frontend stuffs clean up

--- src/app/components/non-admin-loading/non-admin-loading.component.ts
+++ src/app/components/non-admin-loading/non-admin-loading.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component , input } from '@angular/core';
 
 @Component({
   selector: 'app-non-admin-loading',
   imports: [],
   templateUrl: './non-admin-loading.component.html',
   styleUrl: './non-admin-loading.component.scss'
-})
+})  
 export class NonAdminLoadingComponent {
-
+  show = input(false, { alias: 'show'});
 }
